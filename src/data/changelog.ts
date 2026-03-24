4e266a88c145bec939e7440772a32325766c3551
Brian Wicks <me@brianwicks.co.uk>
2026-03-24 16:15:46 +0000

    Update to v1.5.3

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -11,6 +11,15 @@ export interface ChangelogEntry {
 
 export const changelogEntries: ChangelogEntry[] = [
   {
+    version: '1.5.3',
+    date: '2026-03-24',
+    sections: {
+      fixed: [
+        'Fix focus on player number when adding to roster.',
+      ],
+    },
+  },
+  {
     version: '1.5.2',
     date: '2026-03-24',
     sections: {
